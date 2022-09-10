ba47f62497f775dc2e1de63488cb2be1b47a2b1f
Kerri Smith <kerri.p.h.smith@gmail.com>
2022-09-10 08:41:38 -0400

    Completed daily challenge

---
+++ Day2.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# Read ‘n’ and generate a pattern given below
+# Pattern
+#   1
+#   1 2
+#   1 2 3
+#   1 2 3 4
+# Prerequisites:
+#   How to run a loops in shell scripts.
+#   How to execute a bash script.
+#   How to change execute permission of a file.
+# Objective:
+#   To understand the working of loops in a script.
+# Requirements:
+#   Read a value from user
+#   Create a pattern as mentioned above.
+#   Be sure to save a copy to compare with the response on the next screen.
+patternN = ""
+echo -e "Welcome to loops. \n Yor interger \"n\" will generate a loop that prints the pattern \"1 . . . . n\" \n"
+echo -e "Please enter an integer to loop:"; read intN;
+
+if ! [ "$intN" -eq "$intN" ] 2> /dev/null
+        then
+            echo "Sorry integers only"
+            exit 1
+fi
+
+if [ "$intN" -ge 0 ]
+then
+for ((i=1; i<=$intN; i++))
+do
+patternN+=" $i"
+done
+echo -e "Pattern: $patternN"
+fi
+
+if [ "$intN" -lt 0 ]
+then
+for ((i=1; i>=$intN; i--))
+do
+patternN+=" $i"
+done
+echo -e "Pattern: $patternN"
+fi
